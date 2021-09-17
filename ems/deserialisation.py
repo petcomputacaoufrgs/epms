1417be0b288f13d05d85f313862e1e390c621446
Edu Fantini <edufantinicod@gmail.com>
2021-09-17 17:13:54 -0300

    Started working on new deserialization process

--- ems/deserialisation.py
+++ ems/deserialisation.py
@@ -9,10 +9,12 @@ import music21
 
 
 # discover if a list is a continuous sequence
-def is_continuous(list):
+def is_continuous(l):
+    # print(f'list: {list}')
+    # input()
     # if list is empty or len is 1,  return True
-    if len(list) < 2: return True
-    return list[-1] - list[0] == len(list) - 1
+    if len(l) < 2: return True
+    return sorted(l) == list(range(min(l), max(l)+1))
 
 
 # Key index in our keyboard -> M21 Note
@@ -38,61 +40,66 @@ def get_transpose_interval(ks):
 #   Multi Hot Encoding (Pandas DataFrame) -> M21
 #
 # TODO: ligadura, conectar duas measures e somar as durações
-def measure(m_environment, m_performance, SETTINGS, measure_index):
+def measure(m_metric, m_environment, m_performance, SETTINGS):
     if not isinstance(SETTINGS, pd.Series):
         SETTINGS = pd.Series(SETTINGS)
 
-    deserialised_measure = music21.stream.Measure(number=measure_index)
+    # print(m_metric)
+    # input()
 
-    #
-    #   INFO DESERIALISATION
-    #
+    deserialized_measure = music21.stream.Measure(number=int(m_metric.MEASURE.iloc[0]))
 
     # get info about the first frame
     # and considering it as the
     # 'Main' info of the song
     measure_ks = m_environment.KS.mode()[0]
     measure_ts = m_environment.TS.mode()[0]
-    measure_bpm = m_environment.TEMPO.mode()[0]
+    measure_tempo = m_environment.TEMPO.mode()[0]
 
     ks = music21.key.Key(measure_ks)
-    # check for ks change
     transpose_int = get_transpose_interval(ks)
-    deserialised_measure.append(ks)
+    deserialized_measure.append(ks)
 
-    deserialised_measure.append(music21.tempo.MetronomeMark(number=measure_bpm,
+    deserialized_measure.append(music21.tempo.MetronomeMark(number=measure_tempo,
                                                             referent='quarter'))
     ts = music21.meter.TimeSignature(measure_ts)
-    deserialised_measure.append(ts)
-
-    # calculate amount of frames per beat
-    frames_per_beat = SETTINGS.RESOLUTION // ts.numerator
+    deserialized_measure.append(ts)
 
     #
-    #   DATA DECODING
+    #   PERFORM DESERIALIZATION
     #
 
     # decode the measure data, note by note
     for measure_note in m_performance.columns:
+
         # filter the frames where the current note is on
-        on_frames = m_performance.loc[m_performance.loc[:, measure_note] == True, measure_note]
-        on_frames = on_frames.reset_index()
+        on_frames = m_performance.loc[m_performance.loc[:, measure_note] != False, measure_note]
 
         if not on_frames.empty:
 
-            # get the list of on frames
-            frames = [int(f) for f in on_frames.index]
+            # print(f'On frames: {on_frames}')
+            # input()
 
-            # print(frames)
+            # get the list of on frames
+            frames = list(m_metric.loc[on_frames.index].FRAME)
+            # frames = range(1, len(frames) + 1)
+
+            print(f'Frames: {frames}')
             # input()
 
             # if 'frames' is a continuous sequence it can become a single note.
             # if not, it'll become more than one
+
+            # TODO: maybe we should set a threshold on what should become another note
+            # For example: if there's just one OFF frame between two ON frames, the OFF frame would be ignored
+
             temp = []
             while not is_continuous(frames):
+                # print('a')
                 # this will keep track of the frames we
                 # have already counted
                 while is_continuous(temp):
+                    # print('b')
                     # temp is continuous, we'll try to add
                     # the next frame
                     temp.append(frames[0])
@@ -108,17 +115,21 @@ def measure(m_environment, m_performance, SETTINGS, measure_index):
                         # remove from 'temp' the frame that
                         # caused this property loss
                         del temp[-1]
+                    break
 
-                # calculate duration in frames (amount of frames on)
-                n_obj = music21.note.Note(nameWithOctave=measure_note)
-                beat_dur = len(temp) / frames_per_beat  # amount of beats
-                n_obj.duration.quarterLength = abs(beat_dur)
-
-                # get the start frame of the note
-                beat_offset = (frames[0] + 1) % ts.numerator
-
-                # insert into stream
-                deserialised_measure.insert(beat_offset, n_obj)
+                # # calculate duration in frames (amount of frames on)
+                # n_obj = music21.note.Note(nameWithOctave=measure_note)
+                # beat_dur = len(temp) / SETTINGS.RESOLUTION  # amount of beats
+                # n_obj.duration.quarterLength = abs(beat_dur)
+                # print(on_frames.loc[measure_note])
+                # input()
+                # # n_obj.volume.velocityScalar = on_frames[]
+                #
+                # # get the start frame of the note
+                # beat_offset = (frames[0] * SETTINGS.RESOLUTION) / ts.numerator
+                #
+                # # insert into stream
+                # deserialized_measure.insert(beat_offset, n_obj)
 
             #
             #  here list of frames is a continuous sequence
@@ -126,31 +137,36 @@ def measure(m_environment, m_performance, SETTINGS, measure_index):
 
             # calculate duration in quarters
             note_obj = music21.note.Note(nameWithOctave=measure_note)
-            beat_dur = len(frames) / frames_per_beat
+            beat_dur = len(frames) / SETTINGS.RESOLUTION
             note_obj.duration.quarterLength = abs(beat_dur)
 
+            # print(f'Metric at frames index: {m_metric.loc[frames.index]}')
+
             # get the start frame of the note
-            beat_offset = (frames[0] + 1) % ts.numerator
+            beat_offset = (frames[0] * SETTINGS.RESOLUTION) / ts.numerator
+            # beat_offset = (min(m_metric.iloc[frames.index].BEAT) + (min(m_metric.iloc[frames.index].FRAME) / SETTINGS.RESOLUTION))
+            print(f'Beat Offset: {beat_offset}')
+            # input()
 
             # insert into measure
-            deserialised_measure.insert(beat_offset, note_obj)
+            deserialized_measure.insert(beat_offset, note_obj)
 
     # transpose it back to the original ks
-    deserialised_measure.transpose(transpose_int, inPlace=True)
+    deserialized_measure.transpose(transpose_int, inPlace=True)
 
     # insert rests in between the notes
-    deserialised_measure.makeRests(fillGaps=False, inPlace=True)
+    deserialized_measure.makeRests(fillGaps=False, inPlace=True)
 
     # make offsets and durations more strict
     # NOTE: it can remove the 'humanity' of the dynamics
-    # deserialised_measure.quantize(inPlace=True)
+    # deserialized_measure.quantize(inPlace=True)
 
     # return it
-    return deserialised_measure
+    return deserialized_measure
 
 
-# deserialise a PARTxN_NOTESxN_FRAMES array
-def instrument(SETTINGS, INSTRUMENT_BLOCK, ENVIRONMENT_BLOCK, PERFORMANCE_BLOCK, save_as=None):
+# deserialize a instrument line
+def instrument(SETTINGS, INSTRUMENT_BLOCK, METRIC_BLOCK, ENVIRONMENT_BLOCK, PERFORMANCE_BLOCK, save_as=None):
     if not isinstance(SETTINGS, pd.Series):
         SETTINGS = pd.Series(SETTINGS)
 
@@ -159,49 +175,39 @@ def instrument(SETTINGS, INSTRUMENT_BLOCK, ENVIRONMENT_BLOCK, PERFORMANCE_BLOCK,
 
     # set instrument
     try:
-        inst = music21.instrument.instrumentFromMidiProgram(INSTRUMENT_BLOCK.MIDI_CODE[0])
+        inst = music21.instrument.instrumentFromMidiProgram(INSTRUMENT_BLOCK.MIDI_PROGRAM[0])
     except:
         inst = music21.instrument.fromString(INSTRUMENT_BLOCK.INSTRUMENT[0])
 
-    # the following midi codes generate errors
-    # when M21 tries to create the .mid file
-    #
-    # 52 (Voice)
-    # 26 (Steel Electric Guitar)
-    # 29 (Muted Electric Guitar)
-    # 73 (Piccolo)
-    # if inst.midiProgram in [52, 73]:
-    #     inst = instrument.Flute()
-    # elif inst.midiProgram in [26, 29]:
-    #     inst = instrument.ElectricGuitar()
-
+    inst_name = INSTRUMENT_BLOCK.NAME
     inst.autoAssignMidiChannel()
     deserialised_part.insert(0, inst)
 
     # total number of measures (bars)
     # in this part
-    n_measures = len(PERFORMANCE_BLOCK.index) // SETTINGS.RESOLUTION
+    n_measures = max(set(METRIC_BLOCK.MEASURE))
 
-    # deserialise measures
+    # deserialize measures
     #
     # iterate over measures (bars)
     for measure_index in range(1, n_measures + 1):
-        # calculate first and last frame indexes
-        s_frame = (measure_index - 1) * SETTINGS.RESOLUTION
-        e_frame = (measure_index * SETTINGS.RESOLUTION) - 1
 
-        measure_environment = ENVIRONMENT_BLOCK.iloc[np.arange(s_frame, e_frame)]
-        measure_performance = PERFORMANCE_BLOCK.iloc[np.arange(s_frame, e_frame)]
+        print(f'Measure {measure_index}')
 
-        # send measure to deserialisation
-        midi_measure = measure(measure_environment,
+        measure_indexes = METRIC_BLOCK.loc[METRIC_BLOCK['MEASURE'] == measure_index]
+        measure_indexes = measure_indexes.index
+
+        measure_metric = METRIC_BLOCK.iloc[measure_indexes]
+        measure_environment = ENVIRONMENT_BLOCK.iloc[measure_indexes]
+        measure_performance = PERFORMANCE_BLOCK.iloc[measure_indexes]
+
+        # send measure to deserialization
+        midi_measure = measure(measure_metric,
+                               measure_environment,
                                measure_performance,
-                               SETTINGS,
-                               measure_index)
+                               SETTINGS)
 
-        ts_numerator = int(measure_environment.TS[0][0])
-        deserialised_part.insert(measure_index * ts_numerator
-                                 , midi_measure)
+        deserialised_part.insert(measure_index, midi_measure)
 
     deserialised_part.makeNotation(inPlace=True)
 
@@ -211,64 +217,76 @@ def instrument(SETTINGS, INSTRUMENT_BLOCK, ENVIRONMENT_BLOCK, PERFORMANCE_BLOCK,
     return deserialised_part
 
 
-# Deserialise a whole file
-#
-#  Multi Hot Encoding (Pandas DataFrame) -> MIDI
+# deserialize a whole file
 def file(serialised, SETTINGS, save_as=None):
     if not isinstance(SETTINGS, pd.Series):
         SETTINGS = pd.Series(SETTINGS)
 
-    deserialised_score = music21.stream.Score()
+    deserialized_score = music21.stream.Score()
 
     # meta = serialised.metadata
 
     # get a list of unique instruments in the song
     instruments_list = list(set(serialised.index))
     instruments = [serialised.loc[i] for i in instruments_list]
-    # print(instruments)
-    # input()
 
     # separate song parts by instrument
-    for inst_interpretation in instruments:
+    for serial in instruments:
+
+        #   RETRIEVE BLOCKS
+        #   ======||=======
+
         INSTRUMENT_BLOCK = pd.DataFrame(
             {
-                'INSTRUMENT': inst_interpretation.index,
-                'MIDI_CODE': inst_interpretation.MIDI_CODE
+                'INSTRUMENT': serial.index,
+                'NAME': serial.NAME,
+                'MIDI_PROGRAM': serial.MIDI_PROGRAM
+            }
+        )
+
+        METRIC_BLOCK = pd.DataFrame(
+            {
+                'MEASURE': serial.MEASURE,
+                'BEAT': serial.BEAT,
+                'FRAME': serial.FRAME,
             }
         )
 
         ENVIRONMENT_BLOCK = pd.DataFrame(
             {
-                'KS': inst_interpretation.KS,
-                'TS': inst_interpretation.TS,
-                'TEMPO': inst_interpretation.TEMPO,
+                'KS': serial.KS,
+                'TS': serial.TS,
+                'TEMPO': serial.TEMPO,
             }
         )
 
-        inst_drop_list = [i for i in set(INSTRUMENT_BLOCK.columns)]
-        inst_drop_list.remove('INSTRUMENT')
-        PERFORMANCE_BLOCK = inst_interpretation.drop(columns=inst_drop_list)
+        # drop blocks to get performance
+        drop_list = [i for i in set(INSTRUMENT_BLOCK.columns)]
+        drop_list.remove('INSTRUMENT')
+        drop_list = drop_list + [i for i in set(METRIC_BLOCK.columns)]
+        drop_list = drop_list + [i for i in set(ENVIRONMENT_BLOCK.columns)]
+        PERFORMANCE_BLOCK = serial.drop(columns=drop_list).reset_index(drop=True)
 
-        env_drop_list = [i for i in set(ENVIRONMENT_BLOCK.columns)]
-        PERFORMANCE_BLOCK = PERFORMANCE_BLOCK.drop(columns=env_drop_list)
+        # print(PERFORMANCE_BLOCK)
 
         # print('Decoding instrument: {}'.format(INSTRUMENT_BLOCK.NAME))
 
         part = instrument(SETTINGS,
                           INSTRUMENT_BLOCK,
-                          ENVIRONMENT_BLOCK,
-                          PERFORMANCE_BLOCK
+                          METRIC_BLOCK.reset_index(drop=True),
+                          ENVIRONMENT_BLOCK.reset_index(drop=True),
+                          PERFORMANCE_BLOCK.reset_index(drop=True)
                           )
 
         # insert the part at the beginning of the file
-        deserialised_score.insert(0, part)
-        deserialised_score.makeVoices(inPlace=True)
-        # deserialised_score.makeNotation(inPlace=True)
-
-    # decoded.show('text')
+        deserialized_score.insert(0, part)
+        # print(instruments)
+        # input()
+        deserialized_score.makeVoices(inPlace=True)
+        # deserialized_score.makeNotation(inPlace=True)
 
     # save .mid
     if save_as is not None:
-        deserialised_score.write("midi", save_as)
+        deserialized_score.write("midi", save_as)
 
-    return deserialised_score
+    return deserialized_score
